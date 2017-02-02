11264f1be33b5bdfe428bbe161a507db1fd606b6
Danil Vlasov <dvlasov@s-pro.io>
2017-02-02 11:57:37 +0200

    added simple and very dirty getter for area by given coordinates

--- server/index.js
+++ server/index.js
@@ -1,10 +1,49 @@
 import Express from 'express';
-var app = Express();
+import Sequelize from 'sequelize';
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+const app = Express();
+
+const mysqlConn = new Sequelize('mysql://danil@localhost:3306/geo', {});
+
+let mysqlConnectionStatus = null;
+mysqlConn.authenticate()
+  .then((rs) => mysqlConnectionStatus = `Connection OK`)
+  .catch((rs) => mysqlConnectionStatus = `Connection NOK <br>${rs}`)
+
+const prepareModel = (db, model) => {
+  const modelFunc = require(model);
+  return modelFunc(db, Sequelize);
+}
+
+const UsState = prepareModel(mysqlConn, './models/us_state');
+
+app.get('/mysql-connection', (req, res) => {
+  res.send(mysqlConnectionStatus);
 });
 
-app.listen(3000, function () {
+app.get('/mysql/get-area/', (req, res) => {
+  const {
+    lat,
+    lng
+  } = req.query;
+  if (!lat || !lng) {
+    res.status(400).send(`No coords set<br>lat=${lat} | lng=${lng}`);
+    return;
+  }
+
+  UsState.getStateByCoords(lat, lng)
+    .then((rs) => {
+      res.send({state: rs[0]})
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(400).send({
+        error: 'error',
+        payload: err
+      })
+    })
+})
+
+app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
 });
---
+++ server/models/us_state.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var UsState = sequelize.define('UsState', {
+    name: { type: DataTypes.STRING(255), allowNull: false },
+    short_name: { type: DataTypes.STRING(50), allowNull: false },
+    state_id: { type: DataTypes.INTEGER, allowNull: false },
+    geometry: { type: DataTypes.GEOMETRY, allowNull: false },
+  }, {
+    underscored: true,
+    tableName: 'us_state'
+  });
+
+  /**
+   * Return us state by given coords
+   * @param  {Number|String} lat latitude
+   * @param  {Number|String} lng longtitude
+   * @return {Promise|Object} Promise with us state or error object
+   */
+  UsState.getStateByCoords = (lat, lng) => {
+    if (!lat || !lng) return { error: true };
+
+    const QUERY = `
+    SELECT id, name, state FROM us_state
+    WHERE ST_Contains(ST_GeomFromGeoJSON(geometry),
+      ST_GeomFromGeoJSON(CONCAT('{"type": "Point", "coordinates": [', ${lng}, ', ', ${lat}, ']}')));`;
+
+    return sequelize.query(QUERY, { type: sequelize.QueryTypes.SELECT });
+  }
+
+  console.log("UsState =========================================================");
+  return UsState;
+};
