3e6c332477ff9e0ba85466948967dfe13c65cef4
Danil Vlasov <dvlasov@s-pro.io>
2017-02-02 13:51:24 +0200

    added readme and geodata

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 Use MySQL, PostgreSQL and so on to work with geodata
+
+
+```
+CREATE DATABASE geo;
+CREATE TABLE us_state (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  state INT,
+  geometry JSON NOT NULL
+  );
+```
---
+++ data/geodata/prepare_sql_from_json.js
@@ -0,0 +1,23 @@
+fs = require('fs')
+fs.readFile('./gz_2010_us_040_00_20m_low.json', 'utf8', function(err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  try {
+    const parsed = JSON.parse(data);
+    const prepared = parsed.features.map((item) => {
+        return {
+          name: item.properties.NAME,
+          state: item.properties.STATE,
+          geometry: item.geometry
+        }
+      })
+      .map((item) => {
+        return `INSERT INTO us_state (name, state, geometry) VALUES ('${item.name}', '${item.state}', '${JSON.stringify(item.geometry)}');`
+      })
+      .reduce((prev, next) => prev + next + '\n', "");
+    console.log(prepared);
+  } catch (error) {
+    console.log('error', error);
+  }
+});
